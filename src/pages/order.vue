15ec7c80e63fa51e6a2351cf3332c6bdc20fef16
ccqstark <1367305698@qq.com>
2021-05-01 15:12:12 +0800

    framework

---
+++ src/pages/order.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <nav-header></nav-header>
+        <router-view></router-view>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+
+<script>
+    import NavFooter from '../components/NavFooter.vue'
+    import NavHeader from '../components/NavHeader.vue'
+    export default {
+        components: {
+            NavHeader,
+            NavFooter
+        },
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
