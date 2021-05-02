574df9517771423b685d7a24a4e1659d0a113c5e
ccqstark <1367305698@qq.com>
2021-05-02 22:48:48 +0800

    😊 nav-header done

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,12 @@ module.exports = {
         host: "localhost",
         port: 8080,
         proxy: {
-            '/activity': {
-                target: 'https://www.imooc.com',
+            '/api': {
+                target: 'http://mi.futurefe.com',
                 changeOrigin: true,
-                pathRewrite: {
-                    '/activity': '/activity'
-                }
+                // pathRewrite: {
+                //     '/api': ''
+                // }
             }
         }
     }
